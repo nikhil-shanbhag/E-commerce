9e3a8745a7693bbd7b4926c7ae60923156389587
nikhil-shanbhag <nikhilshanbhag35@gmail.com>
2024-01-18 23:28:50 +0530

    Add to cart

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Get all thumbnails and hero images
 let thumbnails = document.querySelectorAll(".thumbnail");
-let heroImages = document.querySelectorAll(".image");
+let images = document.querySelectorAll(".image");
 
 // Add click event listener to each thumbnail
 thumbnails.forEach((thumbnail, index) => {
   thumbnail.addEventListener("click", function () {
-    // Change the src of all hero images to match the clicked thumbnail
-    heroImages.forEach((heroImage) => {
-      heroImage.src = thumbnail.src.replace("-thumbnail", "");
+    // Change the src of all images to match the clicked thumbnail
+    images.forEach((image) => {
+      image.src = thumbnail.src.replace("-thumbnail", "");
     });
   });
 });
@@ -16,13 +16,20 @@ thumbnails.forEach((thumbnail, index) => {
 let bigImage = document.querySelector(".imageCarousel");
 
 function enlarge() {
+  console.log("CLICKED");
   let hidden = document.querySelector(".hiddenContent");
-  hidden.style.visibility = "visible";
+  if (hidden.style.display === "none") {
+    hidden.style.display = "flex";
+    hidden.style.visibility = "visible";
+  } else {
+    hidden.style.display = "none";
+    hidden.style.visibility = "hidden";
+  }
 }
 
 bigImage.addEventListener("click", enlarge);
 
-//Previous next and close
+// Previous, next, and close
 window.onload = function () {
   let currentImageIndex = 0;
   const hiddenImages = Array.from(document.querySelectorAll(".hiddenImage"));
@@ -61,3 +68,91 @@ window.onload = function () {
 
   updateImage();
 };
+
+// Cart object
+let cart = {
+  items: [],
+  total: 0,
+};
+
+// Quantity variable
+let quantity = 0;
+
+// Function to toggle cart visibility
+function toggleCart() {
+  let cart = document.querySelector(".cart .cart-items");
+  cart.style.display = cart.style.display === "none" ? "block" : "none";
+}
+
+// Function to update quantity display
+function updateQuantityDisplay() {
+  document.querySelector(
+    ".quantity"
+  ).innerHTML = `<img src="images/icon-minus.svg" alt="" />${quantity}<img src="images/icon-plus.svg" alt="" srcset="" />`;
+}
+
+// Function to add quantity
+function addQuantity() {
+  quantity++;
+  updateQuantityDisplay();
+}
+
+// Function to remove quantity
+function removeQuantity() {
+  if (quantity > 0) {
+    quantity--;
+    updateQuantityDisplay();
+  }
+}
+
+// Function to handle quantity button click
+function handleQuantityButtonClick(event) {
+  if (event.target.src.includes("icon-plus.svg")) {
+    addQuantity();
+  } else if (event.target.src.includes("icon-minus.svg")) {
+    removeQuantity();
+  }
+}
+
+// Function to add item to cart
+function addToCart() {
+  let quantity = parseInt(document.querySelector(".quantity").innerText);
+  if (quantity > 0) {
+    let item = {
+      name: "Fall Limited Edition Sneakers",
+      price: 125.0,
+      quantity: quantity,
+    };
+    cart.items.push(item);
+    cart.total += item.price * item.quantity;
+    updateCart();
+  }
+}
+
+// Function to update cart
+function updateCart() {
+  let cartItemsDiv = document.querySelector(".cart-items .addeditems");
+  cartItemsDiv.innerHTML = "";
+  cart.items.forEach(function (item, index) {
+    let itemDiv = document.createElement("div");
+    itemDiv.innerHTML = `<img width="60px" src="images/image-product-1-thumbnail.jpg" alt="" />
+                         <p>${item.name} $${item.price.toFixed(2)} x ${
+      item.quantity
+    } $${(item.price * item.quantity).toFixed(2)}</p>
+                         <img class="delete" src="images/icon-delete.svg" alt="" />`;
+    cartItemsDiv.appendChild(itemDiv);
+
+    // Add event listener to the delete button
+    itemDiv.querySelector(".delete").addEventListener("click", function () {
+      cart.items.splice(index, 1);
+      updateCart();
+    });
+  });
+}
+
+// Event listeners
+document.getElementById("cart-icon").addEventListener("click", toggleCart);
+document
+  .querySelector(".quantity")
+  .addEventListener("click", handleQuantityButtonClick);
+document.querySelector(".addToCart").addEventListener("click", addToCart);
